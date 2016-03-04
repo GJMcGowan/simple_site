eb4d0713f0d056a8f820f2a9f8f50dac20bf7041
GJMcGowan <gemcgow@gmail.com>
2016-03-04 21:35:48 +0000

    Adding basic ejs page, as well as backbone package

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var app = express();
+"use strict";
 
+const express = require('express');
+const app = express();
+
+// Port is either from environment or a default
 app.set('port', (process.env.PORT || 5000));
 
+// Have to set basic render engine
+app.set('view engine', 'ejs');
+
+// Sets basic routes
 app.get('/', function (request, response) {
-  response.send('Hello World!');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
